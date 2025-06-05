e77b58fd6cf9b3b3582755c50a5bbb72dc7adb80
Orstedra <daeusosenna@gmail.com>
2025-06-05 22:56:03 +0200

    Initial commit

--- include/d/a/obj/d_a_obj_god_mark.h
+++ include/d/a/obj/d_a_obj_god_mark.h
@@ -2,6 +2,9 @@
 #define D_A_OBJ_GOD_MARK_H
 
 #include "d/a/obj/d_a_obj_base.h"
+#include "d/col/bg/d_bg_s_sph_chk.h"
+#include "m/m3d/m_anmmatclr.h"
+#include "m/m3d/m_anmtexsrt.h"
 #include "s/s_State.hpp"
 #include "s/s_StateMgr.hpp"
 
@@ -10,10 +13,22 @@ public:
     dAcOgodMark_c() : mStateMgr(*this, sStateID::null) {}
     virtual ~dAcOgodMark_c() {}
 
+    virtual int create() override;
+    virtual int doDelete() override;
+    virtual int draw() override;
+    virtual bool createHeap() override;
+    virtual int actorExecute() override;
+    virtual int actorExecuteInEvent() override;
+
     STATE_FUNC_DECLARE(dAcOgodMark_c, Wait);
     STATE_FUNC_DECLARE(dAcOgodMark_c, Shine);
 
 private:
+    /* 0x330 */ nw4r::g3d::ResFile mRes;
+    /* 0x334 */ m3d::smdl_c mMdl;
+    m3d::anmTexSrt_c mAnmTexSrt;
+    m3d::anmMatClr_c mAnmMatClr;
+    dBgS_SphChk mSphChk;
     /* 0x??? */ STATE_MGR_DECLARE(dAcOgodMark_c);
 };
 
--- src/REL/d/a/obj/d_a_obj_god_mark.cpp
+++ src/REL/d/a/obj/d_a_obj_god_mark.cpp
@@ -1,9 +1,67 @@
 #include "d/a/obj/d_a_obj_god_mark.h"
 
+#include "common.h"
+#include "d/a/obj/d_a_obj_base.h"
+#include "f/f_base.h"
+#include "m/m_vec.h"
+#include "nw4r/g3d/res/g3d_resanmtexsrt.h"
+#include "nw4r/g3d/res/g3d_resfile.h"
+#include "nw4r/g3d/res/g3d_resmdl.h"
+
+#include <cstddef>
+
 SPECIAL_ACTOR_PROFILE(OBJ_GOD_MARK, dAcOgodMark_c, fProfile::OBJ_GOD_MARK, 0x20A, 0, 6);
 
 STATE_DEFINE(dAcOgodMark_c, Wait);
 STATE_DEFINE(dAcOgodMark_c, Shine);
+
+int dAcOgodMark_c::create() {
+    CREATE_ALLOCATOR(dAcOgodMark_c);
+
+    mVec3_c min, max;
+    mMdl.getBounds(&min, &max);
+    boundingBox.Set(min, max);
+
+    return SUCCEEDED;
+}
+int dAcOgodMark_c::doDelete() {
+    return SUCCEEDED;
+}
+int dAcOgodMark_c::draw() {
+    drawModelType1(&mMdl);
+    return SUCCEEDED;
+}
+bool dAcOgodMark_c::createHeap() {
+    rot_copy = params & 0xf;
+    if ((params & 0xf) == 0xf) {
+        rot_copy = 0;
+    }
+
+    mRes = nw4r::g3d::ResFile(getOarcResFile("GodsMark"));
+
+    nw4r::g3d::ResMdl resMdl = mRes.GetResMdl("GodsMark");
+
+    if (!mMdl.create(resMdl, &heap_allocator, 0x120, 1, nullptr)) {
+        return false;
+    } else {
+        nw4r::g3d::ResAnmTexSrt resAnmTexSrt = mRes.GetResAnmTexSrt("GodsMark");
+        if (!mAnmTexSrt.create(resMdl, resAnmTexSrt, &heap_allocator, nullptr, 1)) {
+            nw4r::g3d::ResAnmClr anmClr = mRes.GetResAnmClr("GodsMark_F");
+            if (mAnmMatClr.create(resMdl, anmClr, &heap_allocator, nullptr, 1) == 0) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+int dAcOgodMark_c::actorExecute() {
+    return SUCCEEDED;
+}
+int dAcOgodMark_c::actorExecuteInEvent() {
+    return SUCCEEDED;
+}
 
 void dAcOgodMark_c::initializeState_Wait() {}
 void dAcOgodMark_c::executeState_Wait() {}
